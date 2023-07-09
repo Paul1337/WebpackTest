33b62a864da7428f0185920be88b9fe372329591
Paul <p@x128.ru>
2023-07-09 22:01:05 +0300

    big fix

--- config/webpack/webpack.config.ts
+++ config/webpack/webpack.config.ts
@@ -8,7 +8,7 @@ export default [webpackClientConfig, webpackServerConfig];
 // (ok) 1. move all configs into one place (/config folder)
 // (ok) 2. start express server which should serve our static client side
 // (ok) 3. successfully manually run server script
-// automatically run server script (dev.js) (in template manual script should be defined and server script also need to be defined)
+// (ok) automatically run server script (dev.js) (in template manual script should be defined and server script also need to be defined)
 // hydrate on client
 
 // HMR
